16acb5f54a0892906de4ad0df73f056219d47676
Sidney Owallah <sidneyowallah@gmail.com>
2021-08-29 09:50:14 -0500

    Added indeterminate, selected and count logic

--- src/DeviceList.js
+++ src/DeviceList.js
@@ -1,29 +1,67 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './DeviceList.css';
 import { deviceData } from './deviceData';
 
 export const DeviceList = () => {
 	const selectAll = useRef(null);
 	const selectCheckBox = useRef(deviceData.map(() => null));
+	const tableHeaders = Object.keys(deviceData.map((data) => data)[0]);
 
 	const [checkedState, setCheckedState] = useState([]);
+	const [selectCount, setSelectCount] = useState(0);
+	const [selected, setSelected] = useState([]);
 
-	function onChangeSelectAll() {
+	function onChangeSelectAll(e) {
 		selectCheckBox.current.forEach(
 			(checkItem) => (checkItem.checked = selectAll.current.checked)
 		);
+
+		if (e.target.checked) {
+			setSelectCount(deviceData.length);
+		} else {
+			setSelectCount(0);
+			setSelected([]);
+		}
 	}
 
 	function onChangeCheckBox(e) {
 		const i = selectCheckBox.current.indexOf(e.target);
 		const newState = Array.from(checkedState);
 		newState[i] = e.target.checked;
-		console.log('initial', checkedState);
 		setCheckedState(newState);
-		console.log('after', checkedState);
+
+		if (e.target.checked) {
+			setSelected([...selected, e.target.name]);
+			setSelectCount(selectCount + 1);
+		} else if (!e.target.checked) {
+			const newSelectedArr = selected.filter(
+				(currItem) => currItem !== e.target.name
+			);
+			setSelected(newSelectedArr);
+			setSelectCount(selectCount - 1);
+		}
 	}
 
-	const tableHeaders = Object.keys(deviceData.map((data) => data)[0]);
+	function setIndeterminate() {
+		let allChecked;
+		for (const checkItem of selectCheckBox.current) {
+			if (!checkItem) continue;
+			const checked = checkItem.checked;
+			if (allChecked === undefined) {
+				allChecked = checked;
+			} else if (allChecked !== checked) {
+				allChecked = null;
+				break;
+			}
+		}
+		if (typeof allChecked === 'boolean') {
+			selectAll.current.checked = allChecked;
+			selectAll.current.indeterminate = false;
+		} else {
+			selectAll.current.indeterminate = true;
+		}
+	}
+	useEffect(setIndeterminate);
 
 	return (
 		<table className="list-table">
@@ -39,7 +77,9 @@ export const DeviceList = () => {
 						/>
 					</th>
 					<th>
-						<p className="select-count">count</p>
+						<p className="select-count">
+							{selectCount > 0 ? `Selected ${selectCount}` : 'None Selected'}
+						</p>
 					</th>
 					<th>
 						<div>
@@ -64,14 +104,18 @@ export const DeviceList = () => {
 								ref={(r) => (selectCheckBox.current[key] = r)}
 								type="checkbox"
 								onChange={(event) => onChangeCheckBox(event, data)}
-								checked={checkedState[key]}
 								name={data.name}
 							/>
 						</td>
 						<td className="name-cell">{data.name}</td>
 						<td className="device-cell">{data.device}</td>
 						<td className="path-cell">{data.path}</td>
-						<td className="status-cell">{data.status}</td>
+						<td className="status-cell">
+							{data.status === 'available' ? (
+								<span className="green-dot"></span>
+							) : null}
+							<span>{data.status}</span>
+						</td>
 					</tr>
 				))}
 			</tbody>
